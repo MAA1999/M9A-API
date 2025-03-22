9493f08f292b8924ae1678f476e2186efbd96ef2
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2025-03-23 07:56:50 +0800

    perf: 整理代码结构

--- src/getContent.py
+++ src/getContent.py
@@ -6,45 +6,38 @@ import requests
 CN_CONTENT_URL = "https://notice.sl916.com/noticecp/client/query"
 OTHER_URL = "https://re1999.bluepoch.com/activity/official/websites/information/query"
 
+PATTERNS = {
+    "cn": r'(\d+\.\d+)\s*「([^」]+)」版本活动一览',
+    "en": r'Ver\.\s+(\d+\.\d+)\s+\[([^\]]+)\]\s+Preview',
+    "jp": r'Ver\.\s+(\d+\.\d+)\s*「([^」]+)」情報一覧'
+}   
+
+GAME_IDS = {
+    "cn": 50001,
+    "en": 60001,
+    "jp": 70001
+}
+
 def getContent(resource:str):
     if resource == "cn":
-        pattern = r'(\d+\.\d+)\s*「([^」]+)」版本活动一览'
         data = json.loads(requests.get(url=CN_CONTENT_URL,params={"gameId": 50001, "channelId": 100, "subChannelId": 1009, "serverType": 4}).text)
         if data['msg'] == "成功":
             data = data['data']
-        for item in data:
-            item = item["contentMap"]["zh-CN"]
-            title, content = item['title'], item['content']
-            match = re.search(pattern, title)
-            if match:
-                version = match.group(1)
-                version_name = match.group(2)
-                return True, (version, version_name, content)
-    elif resource == "en":
-        pattern = r'Ver\.\s+(\d+\.\d+)\s+\[([^\]]+)\]\s+Preview'
-        data = json.loads(requests.post(url=OTHER_URL,json={"informationType": 2, "current": 1, "pageSize": 5, "gameId": 60001}).text)
+            for item in data:
+                item = item["contentMap"]["zh-CN"]
+                title, content = item['title'], item['content']
+                match = re.search(PATTERNS["cn"], title)
+                if match:
+                    return True, (match.group(1), match.group(2), content)
+    elif resource in ["en", "jp"]:
+        data = json.loads(requests.post(url=OTHER_URL,json={"informationType": 2, "current": 1, "pageSize": 5, "gameId": GAME_IDS[resource]}).text)
         if data['msg'] == "成功":
             data = data['data']['pageData']
-        for item in data:
-            title, content = item['title'], item['content']
-            match = re.search(pattern, title)
-            if match:
-                version = match.group(1)
-                version_name = match.group(2)
-                return True, (version, version_name, content)
-    elif resource == "jp":
-        pattern = r'Ver\.\s+(\d+\.\d+)\s*「([^」]+)」情報一覧'
-        data = json.loads(requests.post(url=OTHER_URL,json={"informationType": 2, "current": 1, "pageSize": 5, "gameId": 70001}).text)
-        if data['msg'] == "成功":
-            data = data['data']['pageData']
-        for item in data:
-            title, content = item['title'], item['content']
-            match = re.search(pattern, title)
-            if match:
-                version = match.group(1)
-                version_name = match.group(2)
-                return True, (version, version_name, content)
-
+            for item in data:
+                title, content = item['title'], item['content']
+                match = re.search(PATTERNS["en"], title)
+                if match:
+                    return True, (match.group(1), match.group(2), content)
     return False, None
 
 if __name__ == "__main__":
